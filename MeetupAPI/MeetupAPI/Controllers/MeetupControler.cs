adc5fa2796145f430d51e64c4d350608dfa8efeb
Oskar Gacki <ogacki@ess.etisoft.pl>
2021-02-11 16:07:30 +0100

    Poczatek API

---
+++ MeetupAPI/MeetupAPI/Controllers/MeetupControler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MeetupAPI.Entities;
+using System.Threading.Tasks;
+
+namespace MeetupAPI.Controllers
+{
+
+    [Route("api/meerup")]
+    public class MeetupControler : ControllerBase
+    {
+        private readonly MeetupContext _meetupContext;
+
+        public MeetupControler(MeetupContext meetupContext)
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult<List<Meetup>> Get()
+        {
+            var meetups = _meetupContext.Meetups.ToList();
+
+            return meetups;
+        }
+    }
+}
